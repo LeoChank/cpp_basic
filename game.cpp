646744b1eca8b661abf59381938531c595ca5101
LeoChank <102619478+LeoChank@users.noreply.github.com>
2022-04-16 17:01:35 +0300

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+
+
+int main(int argc, char* argv[])
+{
+
+	unsigned int max_value = 9;
+	if (argc == 3)
+	{
+		if (argv[1] == static_cast<std::string>("-max"))
+		{
+			unsigned int value = std::stoi(argv[2]);
+			if (value <= 99 && value > 0)
+			{
+				max_value = value;
+			}
+		}
+
+		if (argv[1] == static_cast<std::string>("-level"))
+		{
+			unsigned int value = std::stoi(argv[2]);
+			if (value == 1)
+				max_value = 9;
+			else if (value == 2)
+				max_value = 19;
+			else
+				max_value = 49;
+
+		}
+
+
+	}
+		else if (argc > 3)
+	{
+		std::cout << "Invalid arguments" << std::endl;
+		return -1;
+	}
+
+
+	const std::string filename = "scores.txt";
+
+	std::cout << "Hello! Enter your nickname: " << std::endl;
+	std::string user_name;
+	std::cin >> user_name;
+
+	std::srand(std::time(nullptr));
+	const int random_value = std::rand() % max_value;
+	std::cout << "Current hidden value in the range: " << "1 - " << max_value << std::endl;
+	
+
+	std::cout << "Enter your guess, " << user_name << "!" << std::endl;
+	int buffer_value = 0;
+	unsigned int attempts = 1;
+	while (true)
+	{
+		std::cin >> buffer_value;
+		if (buffer_value < random_value)
+			std::cout << "Greater than " << buffer_value << std::endl;
+		else if (buffer_value > random_value)
+			std::cout << "Less than " << buffer_value << std::endl;
+		else
+		{
+			std::cout << "Correct! Good work!" << std::endl;
+			std::ifstream in_file{ filename };
+			if (!in_file.is_open()) {
+				std::cout << "Failed to open file for read: " << filename << "!" << std::endl;
+				return -1;
+			}
+
+			
+			std::cout << "\n\tHigh scores table:" << std::endl;
+
+
+			std::string input_username;
+			std::ofstream out_file;
+			int high_score = 0;
+			std::string line;
+			unsigned int record = 0;
+			HANDLE handle_stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+			while (true)
+			{
+
+				// Read the username first
+				in_file >> input_username;
+			
+				if (input_username == user_name)
+				{
+					std::streampos pos = in_file.tellg();
+					in_file >> record;
+
+					out_file.open(filename, std::ios_base::binary | std::ios_base::in);
+					if (!out_file.is_open()) {
+						std::cout << "Failed to open file for write: " << filename << "!" << std::endl;
+						return -1;
+					}
+
+					// Append new results to the table:
+
+					if (attempts < record)
+					{
+
+						Beep(500, 500);
+						out_file.seekp(pos, out_file.beg);
+						out_file.seekp(1, out_file.cur);
+						if (attempts < 10)
+						{
+							out_file << static_cast<char>(0x30);
+						}
+							out_file << attempts;
+						SetConsoleTextAttribute(handle_stdOut, FOREGROUND_GREEN);
+
+						std::cout << "Congratulations! You have set a new record!" << std::endl;
+					}
+					
+					out_file.close();
+					break;
+				}
+				// if not find player name
+				if (in_file.eof())
+				{
+
+					out_file.open(filename, std::ios_base::app);
+					if (!out_file.is_open()) {
+						std::cout << "Failed to open file for write: " << filename << "!" << std::endl;
+						return -1;
+					}
+
+					// Append new results to the table:
+					//out_file << user_name << ' ';
+					if (attempts < 10)
+						out_file << "\n" << user_name + " " << (char)0x30 << (char)(attempts + '0');
+					else
+					out_file << "\n" << user_name + " " << attempts;
+					
+					out_file.close();
+					break;
+				}
+
+				// Read the high score next
+				//in_file >> high_score;
+				// Ignore the end of line symbol
+				in_file.ignore();
+
+				if (in_file.fail()) {
+					break;
+				}
+				
+			}
+			SetConsoleTextAttribute(handle_stdOut, FOREGROUND_RED | FOREGROUND_BLUE);
+			std::cout << "<-------------------------------------------------------->" << std::endl;
+			std::cout << user_name << '\t' << "Best result: " << record << std::endl;
+			std::cout << "Current result: " << attempts << std::endl;
+			in_file.close();
+			break;
+
+		}
+
+		++attempts;
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
