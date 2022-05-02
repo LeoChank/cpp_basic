40491c9e50b3ca36c38e495d127d403df1935823
LeoChank <102619478+LeoChank@users.noreply.github.com>
2022-05-02 16:23:14 +0300

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -8,11 +8,8 @@ int read_write_file(const std::string& filename, const std::string& user_name, u
 		std::cout << "Failed to open file for read: " << filename << "!" << std::endl;
 		return -1;
 	}
-
-
 	std::cout << "\n\tHigh scores table:" << std::endl;
-
-
+	
 	std::string input_username;
 	std::ofstream out_file;
 	int high_score = 0;
@@ -21,15 +18,13 @@ int read_write_file(const std::string& filename, const std::string& user_name, u
 	HANDLE handle_stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	while (true)
 	{
-
 		// Read the username first
 		in_file >> input_username;
-
 		if (input_username == user_name)
 		{
 			std::streampos pos = in_file.tellg();
 			in_file >> record;
-
+			
 			out_file.open(filename, std::ios_base::binary | std::ios_base::in);
 			if (!out_file.is_open()) {
 				std::cout << "Failed to open file for write: " << filename << "!" << std::endl;
@@ -53,20 +48,17 @@ int read_write_file(const std::string& filename, const std::string& user_name, u
 
 				std::cout << "Congratulations! You have set a new record!" << std::endl;
 			}
-
 			out_file.close();
 			break;
 		}
 		// if not find player name
 		if (in_file.eof())
 		{
-
 			out_file.open(filename, std::ios_base::app);
 			if (!out_file.is_open()) {
 				std::cout << "Failed to open file for write: " << filename << "!" << std::endl;
 				return -1;
 			}
-
 			// Append new results to the table:
 			//out_file << user_name << ' ';
 			if (attempts < 10)
@@ -77,7 +69,6 @@ int read_write_file(const std::string& filename, const std::string& user_name, u
 			out_file.close();
 			break;
 		}
-
 		// Read the high score next
 		//in_file >> high_score;
 		// Ignore the end of line symbol
@@ -86,7 +77,6 @@ int read_write_file(const std::string& filename, const std::string& user_name, u
 		if (in_file.fail()) {
 			break;
 		}
-
 	}
 	SetConsoleTextAttribute(handle_stdOut, FOREGROUND_RED | FOREGROUND_BLUE);
 	std::cout << "<-------------------------------------------------------->" << std::endl;
