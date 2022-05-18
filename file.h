b2b9b3e3c0cf3e17c645f94e1cee7c0a25a5f92b
LeoChank <102619478+LeoChank@users.noreply.github.com>
2022-05-18 14:10:27 +0300

    Delete file.h

--- file.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <Windows.h>
-
-int read_write_file(const std::string& filename, const std::string& user_name, unsigned int attempts);
-
